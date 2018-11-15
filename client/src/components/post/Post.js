41494c40a69558a046edaddc2ec61c291a825a10
Thanaphon Chaysawat <23chaysawat@Thanaphons-MacBook-Pro.local>
2018-11-15 13:31:07 -0800

    Re-deploy

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../commons/Spinner';
+import { getPost, addComment } from '../../actions/postActions';
+import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
+
+class Post extends Component {
+
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+  render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false}/>
+          <CommentFeed postId={post._id} comments={post.comments} />
+          <CommentForm postId={post._id} />
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back to Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, { getPost })(Post);
